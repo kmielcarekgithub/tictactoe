52f855553f44466622431333349b03c8eedf6181
kmielcarekgithub <tauri3333@gmail.com>
2024-06-10 00:25:01 +0200

    round counter, current user bar and placing symbole multiple times
    added a round counter for the game to be 9 rounds, now current user playing is shown, also prevented player to place symbol more than once in the same box

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const boxes = document.querySelectorAll(".game-square");
 const currUser = document.querySelector("#curr-user");
+const currUserBar = document.querySelector("h2");
 const winningConditions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 
+let roundCounter = 0;
 let symbol = true;
+
 const playerMove = (box) =>{
-    console.log(box.id);
-    boxes[box.id -1].textContent = symbol == true?"X":"O"; 
-    symbol = !symbol;
-    currUser.textContent = symbol == true?"X":"O"
-}
+    if (box.attributes.used.value == "no") {        
+        // console.log(box.attributes.used.value);
+        boxes[box.id -1].textContent = symbol == true?"X":"O"; 
+        currUser.textContent = symbol == true?"O":"X";
+        symbol = !symbol;
+
+        // changing used attribute
+        box.setAttribute("used","yes");
+
+        //changing the current user bar
+        roundCounter++;
+        roundCounter == 9?currUserBar.textContent = "End of a game":"";
+    } else{
+        alert("YOU CAN'T PUT A SYMBOL IN A BOX MORE THAN ONCE!");
+    };
+};
+
 boxes.forEach((box) => {
-    box.addEventListener("click",()=>{playerMove(box)})
+    box.addEventListener("click",()=>{if(roundCounter < 9){playerMove(box)}})
 });
